3bee0893d989bafc177e31cd1c41444cda570af4
nithish26123 <138697337+nithish26123@users.noreply.github.com>
2024-04-07 12:46:56 +0530

    Add files via upload

---
+++ assets/JS/script.js
@@ -0,0 +1,121 @@
+/*===== MENU SHOW =====*/ 
+const showMenu = (toggleId, navId) =>{
+    const toggle = document.getElementById(toggleId),
+    nav = document.getElementById(navId)
+
+    if(toggle && nav){
+        toggle.addEventListener('click', ()=>{
+            nav.classList.toggle('show')
+        })
+    }
+}
+showMenu('nav-toggle','nav-menu')
+
+/*==================== REMOVE MENU MOBILE ====================*/
+const navLink = document.querySelectorAll('.nav__link')
+
+function linkAction(){
+    const navMenu = document.getElementById('nav-menu')
+    // When we click on each nav__link, we remove the show-menu class
+    navMenu.classList.remove('show')
+}
+navLink.forEach(n => n.addEventListener('click', linkAction))
+
+/*==================== SCROLL SECTIONS ACTIVE LINK ====================*/
+const sections = document.querySelectorAll('section[id]')
+
+function scrollActive(){
+    const scrollY = window.pageYOffset
+
+    sections.forEach(current =>{
+        const sectionHeight = current.offsetHeight
+        const sectionTop = current.offsetTop - 50;
+        sectionId = current.getAttribute('id')
+
+        if(scrollY > sectionTop && scrollY <= sectionTop + sectionHeight){
+            document.querySelector('.nav__menu a[href*=' + sectionId + ']').classList.add('active')
+        }else{
+            document.querySelector('.nav__menu a[href*=' + sectionId + ']').classList.remove('active')
+        }
+    })
+}
+window.addEventListener('scroll', scrollActive)
+
+/*===== SCROLL REVEAL ANIMATION =====*/
+const sr = ScrollReveal({
+    origin: 'top',
+    distance: '60px',
+    duration: 2000,
+    delay: 200,
+//     reset: true
+});
+
+sr.reveal('.home__data, .about__img, .skills__subtitle, .skills__text, .wave,  .footer_section, .home__social , .moving-border, .home__tittle, .education, .section-title, .footer-distributed',{}); 
+sr.reveal('.home__img, .about__subtitle, .about__text, .skills__img, .education__content, .footer-left, .h1_contact, .underline, .icon_wrapper',{delay: 400}); 
+sr.reveal('.home__social-icon, .year, .footer-links, .form__',{ interval: 200}); 
+sr.reveal('.skills__data, .work__img, .contact__input, .footer-center, .footer-right, .footer-icons, .name, .email, .telephone, .subject, .message, .submit',{interval: 200}); 
+
+// skill
+
+// dynamic text
+let i = 0;
+
+const randomizeText = () => {
+  const phrase = document.querySelector('.random-word');
+  const compStyles = window.getComputedStyle(phrase);
+  const animation = compStyles.getPropertyValue('animation');
+  const animationTime = parseFloat(animation.match(/\d*[.]?\d+/)) * 1000;
+  
+  const phrases = ['Web designig', 'cloud','web development'];
+  
+  i = randomNum(i, phrases.length);
+  const newPhrase = phrases[i];
+  
+  setTimeout(() => {
+    phrase.textContent = newPhrase;
+  }, 400); // time to allow opacity to hit 0 before changing word
+}
+
+const randomNum = (num, max) => {
+  let j = Math.floor(Math.random() * max);
+  
+  // ensure diff num every time
+  if (num === j) {
+    return randomNum(i, max);
+  } else {
+    return j;
+  }
+}
+
+const getAnimationTime = () => {
+  const phrase = document.querySelector('.random-word');
+  const compStyles = window.getComputedStyle(phrase);
+  let animation = compStyles.getPropertyValue('animation');
+  
+  // firefox support for non-shorthand property
+  animation != "" ? animation : animation = compStyles.getPropertyValue('-moz-animation-duration');
+  
+    // webkit support for non-shorthand property
+  animation != "" ? animation : animation = compStyles.getPropertyValue('-webkit-animation-duration');
+  
+  
+  const animationTime = parseFloat(animation.match(/\d*[.]?\d+/)) * 500;
+  return animationTime;
+}
+
+randomizeText();
+setInterval(randomizeText, getAnimationTime());
+
+
+
+
+
+
+
+
+
+
+
+
+
+
